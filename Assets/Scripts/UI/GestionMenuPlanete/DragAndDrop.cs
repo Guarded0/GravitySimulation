293043d680a31b20ddff5f58ed3034f932d32487
Digiderf <fred.ranger0515@gmail.com>
2025-04-11 15:04:55 -0400

    Bouton pour crée des bouton preset de planet

--- Assets/Scripts/UI/GestionMenuPlanete/DragAndDrop.cs
+++ Assets/Scripts/UI/GestionMenuPlanete/DragAndDrop.cs
@@ -10,36 +10,52 @@ public class DragAndDrop : MonoBehaviour
     private Vector3 coordone;
     private PlanetSettings settingPlaneteACree;
     public LayerMask layerMask;
+    public Transform listBouton;
+    public GameObject prefabBouton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
     void Awake()
-    { 
+    {
     }
     // Update is called once per frame
     void Update()
     {
-        if(activer){
+        if (activer)
+        {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out hit, float.MaxValue, layerMask)){
-                coordone = hit.point; 
-                pointeur.position = coordone; 
+            if (Physics.Raycast(ray, out hit, float.MaxValue, layerMask))
+            {
+                coordone = hit.point;
+                pointeur.position = coordone;
             }
-            if(Input.GetMouseButtonDown(0)){
-                NBodySimulation.Instance.CreatePlanet(coordone, settingPlaneteACree, "New planete");             
-                activer = false;    
-                pointeur.gameObject.SetActive(false);        
+            if (Input.GetMouseButtonDown(0))
+            {
+                NBodySimulation.Instance.CreatePlanet(coordone, settingPlaneteACree, "New planete");
+                activer = false;
+                pointeur.gameObject.SetActive(false);
             }
-            
+
         }
     }
-    
-    public void demarerConstruction(ButtonPrefab buttonPrefab){
+
+    public void demarerConstruction(ButtonPrefab buttonPrefab)
+    {
         activer = true;
         settingPlaneteACree = buttonPrefab.settings;
         pointeur.gameObject.SetActive(true);
+    }
+
+    public void creeBouton()
+    {
+        if (Cible.current != null)
+        {
+            GameObject nouveauBouton = Instantiate(prefabBouton, new Vector3(0, 0, 0), Quaternion.identity);
+            nouveauBouton.GetComponent<ButtonPrefab>().settings = Cible.current.GetComponent<CelestialBody>().planetSettings;
+            nouveauBouton.transform.SetParent(listBouton, false);
+        }
     }
 }
