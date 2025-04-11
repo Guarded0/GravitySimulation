140590544eeb36ed15002263a67b48205cf77323
RamenOsaka <RamenOsaka.mc@gmail.com>
2025-04-11 15:03:30 -0400

    Terminer script sauverage + charger systeme planetaires

--- Assets/Scripts/Managers/SauveragerSysteme.cs
+++ Assets/Scripts/Managers/SauveragerSysteme.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEditor;
+using UnityEngine.InputSystem;
 
 
 public class SauveragerSysteme : MonoBehaviour
@@ -13,28 +13,64 @@ public class SauveragerSysteme : MonoBehaviour
     }
 
     void SauveragerSystemePlanetaire() {
-        List<PlanetSettings> listeDonnesPlanetes = new List<PlanetSettings>{};
-        DonneesPlanetes donneesPlanetes = new DonneesPlanetes();
+        DoneesScene doneesScene = new DoneesScene();
 
         foreach (CelestialBody planete in NBodySimulation.celestialBodies) {
-            listeDonnesPlanetes.Add(planete.planetSettings);
+            doneesScene.listeDonneesPlanetes.Add(planete.planetSettings);
+            doneesScene.listeCoordonnees.Add(planete.transform.position);
         }
-        donneesPlanetes.listeDonneesPlanetes = listeDonnesPlanetes;
 
-        string donneesSysteme = JsonUtility.ToJson(donneesPlanetes, true);
+        doneesScene.gravConstant = NBodySimulation.Instance.gravConstant;
+        doneesScene.planetGravity = NBodySimulation.Instance.planetGravity;
+
+        if (NBodySimulation.Instance.relativeBody != null) {
+        doneesScene.relativeBody = NBodySimulation.Instance.relativeBody.name;
+        }
+
+        string donneesSysteme = JsonUtility.ToJson(doneesScene, true);
         System.IO.File.WriteAllText(filePath, donneesSysteme);
         Debug.Log(filePath);
     }
 
-    
-    public struct DonneesPlanetes {
-        public List<PlanetSettings> listeDonneesPlanetes;
-    } 
+    void ChargerSystemePlanetaire() {
+        foreach(CelestialBody planete in NBodySimulation.celestialBodies) {
+            Destroy(planete.gameObject);
+        }
+
+        string donneesSysteme = System.IO.File.ReadAllText(filePath);
+        DoneesScene donneesCharger = JsonUtility.FromJson<DoneesScene>(donneesSysteme);
+
+        for (int i = 0; i < donneesCharger.listeDonneesPlanetes.Count; i++) {
+            NBodySimulation.Instance.CreatePlanet(donneesCharger.listeCoordonnees[i], donneesCharger.listeDonneesPlanetes[i]);
+            
+            if (donneesCharger.relativeBody != null) {
+                NBodySimulation.Instance.relativeBody.name = donneesCharger.relativeBody;
+            }
+        }
+
+        NBodySimulation.Instance.gravConstant = donneesCharger.gravConstant;
+        NBodySimulation.Instance.planetGravity = donneesCharger.planetGravity;
+
+        
+
+    }
+
+    [System.Serializable]
+    public class DoneesScene {
+        public List<PlanetSettings> listeDonneesPlanetes = new List<PlanetSettings>{};
+        public List<Vector3> listeCoordonnees = new List<Vector3>{};
+
+        public float gravConstant;
+        public bool planetGravity;
+        public string relativeBody;
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.K)) {
             SauveragerSystemePlanetaire();
+        } if (Input.GetKeyDown(KeyCode.L)) {
+            ChargerSystemePlanetaire();
         }
     }
 }
