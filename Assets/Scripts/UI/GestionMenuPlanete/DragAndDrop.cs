27acaf6b2b2beb5a39a07af28fa96260bb98b8b9
Digiderf <fred.ranger0515@gmail.com>
2025-04-20 20:40:14 -0400

    Fix bug menu planète

--- Assets/Scripts/UI/GestionMenuPlanete/DragAndDrop.cs
+++ Assets/Scripts/UI/GestionMenuPlanete/DragAndDrop.cs
@@ -16,7 +16,7 @@ public class DragAndDrop : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        afficherBoutonPlanet(); 
     }
     void Awake()
     {
