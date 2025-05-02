7ed7c12ae12424f5af71c44031539cfece015425
ArianeLepage <ariane.lepage28@gmail.com>
2025-05-02 16:48:28 -0400

    paramètres
    plen écran et fps

--- Assets/Scripts/UI/MenuPrincipal/resolution.cs
+++ Assets/Scripts/UI/MenuPrincipal/resolution.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
 using TMPro;
+using System;
+using UnityEngine.UI;
+using System.Xml;
 //source : https://www.youtube.com/watch?v=HnvPNoU9Wjw 
+public class Parametres{
+    public Resolution resolution = new Resolution(); 
+    public Boolean pleinEcran = false;
+    public Boolean fps = false;
+    public float sensibiliter = 100; 
+    }
 public class resolution : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
+    public Toggle togglePleinEcran;
+    public Toggle toogleFPS;
+    public TMP_Text textFPS;
 
     private Resolution[] resolutions;
 
+    public static Parametres parametres = new Parametres();
+
+
+    public float chronometre = 0;
+    public int frameCount = 0;
+
     void Start()
     {
+
         // Obtenir toutes les résolutions disponibles
         resolutions = Screen.resolutions;
 
@@ -21,7 +40,7 @@ public class resolution : MonoBehaviour
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "Hz";
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRateRatio + "Hz";
             if (!options.Contains(option)) 
                 options.Add(option);
 
@@ -39,12 +58,45 @@ public class resolution : MonoBehaviour
 
         // Ajouter l'écouteur d'événement
         resolutionDropdown.onValueChanged.AddListener(SetResolution);
+
+        togglePleinEcran.onValueChanged.AddListener(SetPleinEcran);
+        toogleFPS.onValueChanged.AddListener(ShowFPS);
+    }
+    void Update()
+    {
+        if(parametres.fps == true ) {
+        updateFPS();
+        }
     }
 
     public void SetResolution(int index)
     {
-        Resolution selected = resolutions[index];
-        Screen.SetResolution(selected.width, selected.height, true);
-        Debug.Log("Résolution changée : " + selected.width + "x" + selected.height);
+        parametres.resolution = resolutions[index];
+        Screen.SetResolution(parametres.resolution.width, parametres.resolution.height, true);
+        Debug.Log("Résolution changée : " + parametres.resolution.width + "x" + parametres.resolution.height);
     }
+
+    public void SetPleinEcran(Boolean boolPleinEcran){
+        parametres.pleinEcran = boolPleinEcran; 
+        Screen.fullScreen = parametres.pleinEcran; // Met en plein écran
+        Debug.Log("Plein Écran: " + parametres.pleinEcran);
+
+    }
+
+    public void ShowFPS(Boolean boolFPS){
+        parametres.fps = boolFPS;
+        textFPS.gameObject.SetActive(parametres.fps);
+        Debug.Log("FPS: " + parametres.fps);
+    }
+    public void updateFPS(){
+        frameCount += 1;
+        chronometre += Time.deltaTime;
+            
+        if(chronometre >= 1){ 
+            textFPS.text = "FPS: " + Mathf.Round(frameCount/chronometre);
+            chronometre = 0;
+            frameCount = 0;
+            }
+        }
 }
+
