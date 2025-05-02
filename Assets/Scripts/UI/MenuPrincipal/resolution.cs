15a2da09e6781ffb4f589b80f9937cdec148c2ce
ArianeLepage <ariane.lepage28@gmail.com>
2025-05-02 17:27:09 -0400

    slider camera
    slider vitesse camera fonctionnel

--- Assets/Scripts/UI/MenuPrincipal/resolution.cs
+++ Assets/Scripts/UI/MenuPrincipal/resolution.cs
@@ -3,6 +3,7 @@ using TMPro;
 using System;
 using UnityEngine.UI;
 using System.Xml;
+using Unity.VisualScripting;
 //source : https://www.youtube.com/watch?v=HnvPNoU9Wjw 
 public class Parametres{
     public Resolution resolution = new Resolution(); 
@@ -10,20 +11,23 @@ public class Parametres{
     public Boolean fps = false;
     public float sensibiliter = 100; 
     }
+
 public class resolution : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
     public Toggle togglePleinEcran;
     public Toggle toogleFPS;
     public TMP_Text textFPS;
+    public Slider sliderSensibiliter;  
+    public TMP_InputField inputFieldSensibiliter;
 
     private Resolution[] resolutions;
 
     public static Parametres parametres = new Parametres();
 
 
-    public float chronometre = 0;
-    public int frameCount = 0;
+    private float chronometre = 0;
+    private int frameCount = 0;
 
     void Start()
     {
@@ -61,6 +65,8 @@ public class resolution : MonoBehaviour
 
         togglePleinEcran.onValueChanged.AddListener(SetPleinEcran);
         toogleFPS.onValueChanged.AddListener(ShowFPS);
+        sliderSensibiliter.onValueChanged.AddListener(setSensibiliteAvecSlider);
+        inputFieldSensibiliter.onValueChanged.AddListener(setSensibiliteAvecInputField);
     }
     void Update()
     {
@@ -82,6 +88,21 @@ public class resolution : MonoBehaviour
         Debug.Log("Plein Écran: " + parametres.pleinEcran);
 
     }
+    public void setSensibiliteAvecInputField(string StringValeur)
+    {
+        float valeur = float.Parse(StringValeur);
+        valeur = Math.Max(valeur, 0);
+        valeur = Math.Min(valeur, 200);
+        parametres.sensibiliter = valeur;
+
+        sliderSensibiliter.value = valeur;
+    }
+    public void setSensibiliteAvecSlider(float valeur)
+    {
+        parametres.sensibiliter = valeur;       
+        inputFieldSensibiliter.text = valeur.ToString("F1"); // Mettre jour l'input field
+    }    
+
 
     public void ShowFPS(Boolean boolFPS){
         parametres.fps = boolFPS;
