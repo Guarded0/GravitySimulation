5c56489958008a5cab27561bccc12ff4e9638c26
Guarded0 <ricardoarb007@gmail.com>
2025-05-09 14:35:32 -0400

    fix boutton

--- Assets/Scripts/UI/UpdateMenu.cs
+++ Assets/Scripts/UI/UpdateMenu.cs
@@ -1,36 +1,28 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class UpdateMenu : MonoBehaviour
 {
     private Boolean activer =  true; 
     public Vector3 positionOuvert = new Vector3();
     public Vector3 positionFermer = new Vector3();
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-    void Awake()
-    {
-      
-       
-    }
+    private TMP_Text text;
 
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        GetComponent<Button>().onClick.AddListener(updateEtatMenu);
+        text = GetComponentInChildren<TMP_Text>();
     }
     public void updateEtatMenu(){
          if(activer){
-            LeanTween.move(gameObject.GetComponent<RectTransform>(), positionFermer, 0.5f).setEase(LeanTweenType.easeOutExpo);
+            LeanTween.move(transform.parent.GetComponent<RectTransform>(), positionFermer, 0.5f).setEase(LeanTweenType.easeOutExpo);
+            text.text = ">";
             activer = false;
         } else{
-            LeanTween.move(gameObject.GetComponent<RectTransform>(), positionOuvert, 0.5f).setEase(LeanTweenType.easeOutExpo);
+            LeanTween.move(transform.parent.GetComponent<RectTransform>(), positionOuvert, 0.5f).setEase(LeanTweenType.easeOutExpo);
+            text.text = "<";
             activer = true;  
         }
 
