9670ef4d1dc23f76032b735a5dd1df146ba89b0b
Digiderf <fred.ranger0515@gmail.com>
2025-05-21 11:23:39 -0400

    Ajout de la source de Grid
    J'ai faut un script vide pour citer la vidéo que j'ai utiliser pour faire le shader grid

---
+++ Assets/SourceShader.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class SourceShader : MonoBehaviour
+{
+    //Source : https://www.youtube.com/watch?v=nKF4Pwb-6Ow&list=WL&index=3 
+    //Cette source a été utilisée pour crée le shader Grid qui est utiliser pour crée la texture du gameObject Plan 
+    
+}
